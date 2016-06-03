ac77a3e6299545e6c1f9ddbca1b97f39313ce0cf
Kocherga Vitalii <ko4erha@gmail.com>
2016-06-03 17:32:29 +0300

    Design patterns. Strategy

--- src/tutorial/strategy/entities/BubbleSort.java
+++ src/tutorial/strategy/entities/BubbleSort.java
@@ -1,13 +1,16 @@
 package tutorial.strategy.entities;
 
+import java.util.Arrays;
+
 /**
  * Concrete strategy. Bubble sorting algorithm.
  * Created by Kocherha Vitalii on 31.05.2016.
  */
 public class BubbleSort implements Sorting {
-
+    int cicles = 0;
     @Override
     public void sort(int[] arr) {
+        System.out.println("Bubble sorting");
         for (int i = arr.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -15,7 +18,13 @@ public class BubbleSort implements Sorting {
                     arr[j] = arr[j + 1];
                     arr[j + 1] = bubble;
                 }
+                cicles++;
             }
+            cicles++;
         }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println("number of cicles: " + cicles);
     }
+
 }
